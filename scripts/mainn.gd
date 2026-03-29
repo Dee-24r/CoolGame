7f627a65f234bb9d70b773bb2e56df1fb285cca7
Funmilayo <fummyoshe@gmail.com>
2026-03-29 19:39:02 -0400

    GoGame v.1.0

---
+++ scripts/mainn.gd
@@ -0,0 +1,42 @@
+extends Node2D
+var score: int = 0
+@onready var score_label: Label = $display/scorePanel/score_Label
+@onready var winning_sound: AudioStreamPlayer2D = $LevelRoot/player/winningSound
+signal win
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_setup_level()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func _setup_level() -> void:
+	#bananas
+	var bananas = $LevelRoot.get_node_or_null("bananas")
+	if bananas:
+		for banana in bananas.get_children():
+			banana.collected.connect(_increase_score)
+	
+	#snail enemies
+	var enemies = $LevelRoot.get_node_or_null("enemies")
+	if enemies:
+		for enemy in enemies.get_children():
+			enemy.player_failed.connect(_on_player_failed)
+
+#signals
+func _on_player_failed(body):
+	body.fail()
+	print("Player failed")
+
+func _winner() -> void:
+	emit_signal("win")
+	winning_sound.play()
+
+func _increase_score() -> void:
+	score += 5
+	score_label.text = "SCORE: %s" % score
+	if score == 20:
+		_winner()
+	
